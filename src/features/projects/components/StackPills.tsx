58b5f5a15bef314042fafa24bae35ee857455567
Alex Arraga <arraga.alex@gmail.com>
2025-08-17 18:03:18 -0300

    bg golang

--- src/features/projects/components/StackPills.tsx
+++ src/features/projects/components/StackPills.tsx
@@ -4,8 +4,8 @@ interface Props {
 
 export const StackPills = ({ name }: Props) => {
   return (
-    <div className={`flex justify-center items-center py-1 px-3 rounded-full capitalize w-fit
-      ${name === 'go' ? 'bg-sky-400 text-white' : 'bg-white text-black'}`
+    <div className={`flex justify-center items-center py-1 px-3 rounded-full w-fit
+      ${name === 'Go' ? 'bg-golang text-white' : 'bg-white text-black'}`
     }>
       {name}
     </div>
