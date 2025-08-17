31d7f6f5511818e014dc14ccf317ae9e205022e5
Alex Arraga <arraga.alex@gmail.com>
2025-08-17 17:16:57 -0300

    project status fixed

--- src/features/projects/components/ProjectStatus.tsx
+++ src/features/projects/components/ProjectStatus.tsx
@@ -4,17 +4,19 @@ interface Props {
 }
 
 export const ProjectStatus = ({ status, withText = false }: Props) => {
+  const circleStatusClass = `w-2.5 h-2.5 rounded-full ${status === 'production' ? 'bg-blue-300' : 'bg-teal-300'}`
+
   return (
     <>
       {withText ? (
         <div className={"flex justify-center items-center gap-2 w-fit"}>
-          <div className={`w-2.5 h-2.5 rounded-full ${status === 'production' ? 'bg-blue-300' : 'bg-teal-300'}`} />
+          <div className={circleStatusClass} />
           <p>
             {status === 'production' ? 'Production' : 'Personal'}
           </p>
         </div>
       ) : (
-        <div className='w-2.5 h-2.5 rounded-full bg-indigo-300' />
+        <div className={circleStatusClass} />
       )}
     </>
   )
