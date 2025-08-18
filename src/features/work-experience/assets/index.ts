640cea4770cde85c55f9a917dc8d42adaff6a5e7
Alex Arraga <arraga.alex@gmail.com>
2025-08-18 11:20:53 -0300

    images added

---
+++ src/features/work-experience/assets/index.ts
@@ -0,0 +1,5 @@
+const basePath = '/images/work-experience'
+
+export const GarageGoImage = `${basePath}/garage-go.png`
+export const GestionaTusDatosImage = `${basePath}/gestionatusdatos.png`
+export const PodoVipImage = `${basePath}/podovip.png`
