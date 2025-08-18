069230ee8e66119c3be88c817ea267fb6b6aa163
Alex Arraga <arraga.alex@gmail.com>
2025-08-18 12:39:54 -0300

    wip

--- src/app/work-experience/page.tsx
+++ src/app/work-experience/page.tsx
@@ -7,11 +7,14 @@ export default function WorkExperiencePage() {
       <section className="container-section">
         <h1 className="h1-semibold">Work Experience</h1>
 
-        <div className="grid grid-cols-3 justify-items-start items-start gap-10">
-          {WorkExperiences.map((data, index) => (
-            <WorkExperienceCard key={data.name + index} {...data} />
-          ))}
+        <div className="div-central mt-16">
+          <div className="grid grid-cols-3 justify-items-start items-start gap-10">
+            {WorkExperiences.map((data, index) => (
+              <WorkExperienceCard key={data.name + index} {...data} />
+            ))}
+          </div>
         </div>
+
       </section>
     </main>
   );
