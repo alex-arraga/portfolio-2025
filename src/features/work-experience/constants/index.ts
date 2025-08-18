da732d5256ebeedd94692ad01febe4310fe9eba0
Alex Arraga <arraga.alex@gmail.com>
2025-08-18 11:22:13 -0300

    work experience images added in cosntants

--- src/features/work-experience/constants/index.ts
+++ src/features/work-experience/constants/index.ts
@@ -1,11 +1,13 @@
-const WorkExperiences: WorkExperience[] = [
+import { GarageGoImage, GestionaTusDatosImage, PodoVipImage } from "../assets";
+
+export const WorkExperiences: WorkExperience[] = [
   {
     name: 'Garage Go',
     description: 'Empresa automotriz chilena que ofrece servicios de mantenimiento vehicular con retiro y entrega a domicilio. Se presentan como “la primera startup automotriz” en Chile en ofrecer una infraestructura de soluciones para un servicio completo de movilidad eficiente.',
     role: ['Full Stack Developer'],
     date: '21/04/25 - Actualidad',
     manager: 'Javier Palma | +56 9 34172037',
-    image: ''
+    image: GarageGoImage
   },
   {
     name: 'Gestiona Tus Datos',
@@ -13,7 +15,7 @@ const WorkExperiences: WorkExperience[] = [
     role: ['Full Stack Developer', 'Diseñador UX/UI'],
     date: '21/04/25 - Actualidad',
     manager: 'Javier Palma | +56 9 34172037',
-    image: ''
+    image: GestionaTusDatosImage
   },
   {
     name: 'Podo Vip',
@@ -21,6 +23,6 @@ const WorkExperiences: WorkExperience[] = [
     role: ['Full Stack Developer'],
     date: '21/04/25 - Actualidad',
     manager: 'Ricardo | +56 9 56321548',
-    image: ''
+    image: PodoVipImage
   },
 ]
